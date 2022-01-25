56e5dfc1efdd01736c7a411cd5bdad37655fb562
Cpache1 <capacheco111@outlook.com>
2022-01-25 19:06:28 +0000

    Latest changes doc

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -159,10 +159,10 @@ public class LevelManager : MonoBehaviour {
         agent.GetComponent<FieldOfView>().targetDetected = false;
         agent.GetComponent<ProximityDetector>().playerDetected = false;
         agentStatus.lastVisibleTargetPosition = agentStartPosition;
+        
         //Dirty workaround to stop the agent from following the previous path. 
         //For some reason the coroutine started by the same expression was not able to stop, so just call a new path request that instantly exits.
         //PathRequestManager.RequestPath(transform.position, agentStartPosition, agentStatus.OnPathFound);
-
         PathController agentPathController = agent.GetComponent<PathController>();
         agentPathController.RequestPath(transform.position, agentStartPosition, agentStatus.OnPathFound);
 
