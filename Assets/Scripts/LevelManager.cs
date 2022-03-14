3bc841b0650bc297c4d45da7f8ed311debb707dc
Cpache1 <c.pacheco@qmul.ac.uk>
2022-03-14 18:36:11 +0000

    Update to LevelManager.cs
    - Added Time Management and State Management code.
    - It can now retrieve the current state.

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Events;
 using Monte;
+using System.Linq;
 
 public class LevelManager : MonoBehaviour {    
     public bool moderatorActive;
@@ -32,10 +33,20 @@ public class LevelManager : MonoBehaviour {
     GameObject player;
 
     //For Algorithm stuff
+    private GameObject cursor;
     int algorithmIndx; 
     AIAgent agentType;
     Model model;
     string settings;
+    float[] stateRep;
+    private int tick;
+    private int idleTime;
+    private int fullTime;
+    private List<string> keyPresses;
+
+    private Vector3 previousPlayerPosition;
+    private Vector3 previousBotPosition;
+    private Vector3 previousCursorPosition;
 
     //Game Events
     public UnityEvent OnGameStart;
@@ -52,8 +63,9 @@ public class LevelManager : MonoBehaviour {
     private Color fpsIndicatorColor;
     private Color fpsWarningColor = new Color(0.9f, 0, 0.2f, 1);
 
-    private void Awake() {        
-        if(doCountDown) {
+    private void Awake() {
+        keyPresses = new List<string>();
+        if (doCountDown) {
             if (!silentCountDown) {
                 timer = GameObject.Find("Timer").GetComponent<Text>();
             }
@@ -67,6 +79,8 @@ public class LevelManager : MonoBehaviour {
 
         if (agent != null) {
             agentStartFrustration = agent.GetComponent<FrustrationComponent>().levelOfFrustration;
+            stateRep = new float[57];
+            tick = 0;
         }
 
         if (player != null) {
@@ -93,6 +107,18 @@ public class LevelManager : MonoBehaviour {
     IEnumerator LateStart() {
         yield return new WaitForFixedUpdate();
         OnGameStart.Invoke();
+
+        if (player.GetComponent<PlayerController>().isAI)
+        {//CHEAT! When the player is AI the cursor is "always" on the enemy
+            cursor = GameObject.Find("Monster"); 
+        }
+        else
+        {
+            cursor = GameObject.Find("GunControls");
+        }
+        previousPlayerPosition = player.transform.position;
+        previousCursorPosition = cursor.transform.position;
+        previousBotPosition = agent.transform.position;
         yield return null;
     }
 
@@ -122,6 +148,12 @@ public class LevelManager : MonoBehaviour {
 
     //TODO: Reset will needs additions for latest AI (MCTS)
     public void ResetStage(int reward) {
+        //algorithm related
+        tick = 0;
+        keyPresses.Clear();
+        for (int n = 0; n < stateRep.Length; n++)
+            stateRep[n] = 0.0f;
+
         GameObject[] currentBombs = GameObject.FindGameObjectsWithTag("bomb");
         for (int i = 0; i < currentBombs.Length; i++) {
             //Defusing bombs before destroy prevents them spawing fires
@@ -187,6 +219,8 @@ public class LevelManager : MonoBehaviour {
     }
 
     private void FixedUpdate() {
+        tick++;
+
         if (doCountDown) {
             countdown -= Time.deltaTime;
             if (countdown < 0) {
@@ -209,9 +243,30 @@ public class LevelManager : MonoBehaviour {
             scoreBoard.text = score.ToString() + " SCORE";
 
         }
+
+        //ADDED FOR ALGORITHM STUFF
+        if (!player.GetComponent<PlayerController>().isAI)
+        {
+            foreach (KeyCode kcode in Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKey(kcode))
+                {
+                    keyPresses.Add(kcode.ToString());
+                }
+            }
+        }
+
+        //TODO: There needs to be a way of checking idle time when the player is an AI...
+        if (!Input.anyKey)
+        {
+            //Starts counting when no button is being pressed
+            idleTime = idleTime + 1;
+        }
+        fullTime = fullTime + 1;
     }
 
     public void LoadLevel(string name) {
+        tick = 0;
         // Moderator is a research assistant who has to hold down Ctrl for key commands to work.
         if (moderatorActive) {
             if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)) {
@@ -221,5 +276,166 @@ public class LevelManager : MonoBehaviour {
         if (!moderatorActive || (moderatorActive && name == "Tutorial")) {
             SceneManager.LoadScene(name);
         }
+    }
+
+    //======================STATE MANAGEMENT============================
+    /// <summary>
+    /// Will return the current state by updating stateRep array.
+    /// HACK: It follows a specific order - same as model, as it needs to have the same 
+    /// elements in the same place. 
+    /// DANGER: Careful with changing said order. 
+    /// </summary>
+    /// <returns></returns>
+    public float[] GetCurrentState()
+    {
+        stateRep[0] = ((float)idleTime / (float)fullTime); //idleTime 
+        stateRep[1] = score; //score 
+
+        //AGENT RELATED
+        if (!agent.Equals(null))
+        {
+            stateRep[2] = ((agent.transform.position - previousBotPosition).magnitude); //botDistanceTraveled
+            previousBotPosition = agent.transform.position;
+
+            stateRep[3] = agent.transform.position.x; //botPositionX
+            stateRep[4] = agent.transform.position.y; //botPositionY
+            stateRep[5] = agent.transform.eulerAngles.z; //botRotation
+            stateRep[6] = agent.GetComponent<Movement>().speed; //botSpeed
+            stateRep[7] = agent.GetComponent<Movement>().rotationSpeed; //botRotationSpeed
+            stateRep[8] = agent.GetComponent<FieldOfView>().viewAngle; //botViewAngle
+            stateRep[9] = agent.GetComponent<FieldOfView>().viewRadius; //botViewRadius
+            stateRep[10] = agent.GetComponent<Movement>().isWaiting ? 1 : 0; //botSearching
+            stateRep[11] = agent.GetComponent<Movement>().lookAroundCycle; //botSearchTurns
+            stateRep[12] = agent.GetComponent<ProximityDetector>().hearingRadius; //botHearingRadius
+            stateRep[13] = agent.GetComponent<ProximityDetector>().detectionChance; //botHearingProbability
+            stateRep[14] = agent.GetComponent<Health>().health; //botHealth
+            stateRep[15] = agent.GetComponent<FrustrationComponent>().levelOfFrustration; //botFrustration
+            stateRep[16] = agent.GetComponent<Movement>().riskTakingFactor; //botRiskTakingFactor
+            stateRep[17] = agent.GetComponent<Movement>().takingRiskyPath ? 1 : 0; //botTakingRiskyPath
+
+            int targetSeen = (agent.GetComponent<ProximityDetector>().playerDetected || agent.GetComponent<FieldOfView>().targetDetected) ? 1 : 0;
+            stateRep[18] = targetSeen; //botSeeingPlayer
+            int botInChase = agent.GetComponent<Movement>().targetLastSeen ? 1 : 0;
+            stateRep[19] = botInChase; //botChasingPlayer
+
+            stateRep[46] = agent.GetComponent<Health>().isBurning ? 1 : 0; //botBurning
+            stateRep[47] = agent.GetComponent<Health>().deltaHealth; //botDeltaHealth
+            stateRep[48] = agent.GetComponent<Health>().botDied ? 1 : 0; //botDied
+
+            float cursorDistanceFromBot = (new Vector2(cursor.transform.position.x, cursor.transform.position.y) -
+                       new Vector2(agent.transform.position.x, agent.transform.position.y)).magnitude;
+            stateRep[22] = cursorDistanceFromBot; //cursorDistanceFromBot
+
+        }
+        // AGENT AND PLAYER RELATED
+        if (!agent.Equals(null) && !player.Equals(null))
+        {
+            float botDistanceFromPlayer = (new Vector2(agent.transform.position.x, agent.transform.position.y) -
+                new Vector2(player.transform.position.x, player.transform.position.y)).magnitude;
+            stateRep[20] = botDistanceFromPlayer; //botDistanceFromPlayer
+        }
+
+        //PLAYER RELATED
+        if (!player.Equals(null))
+        {
+            stateRep[23] = ((player.transform.position - previousPlayerPosition).magnitude); //playerDistanceTravelled
+            previousPlayerPosition = player.transform.position;
+
+            stateRep[24] = player.transform.position.x; //playerPositionX
+            stateRep[25] = player.transform.position.y; //playerPositionY
+            stateRep[26] = player.transform.eulerAngles.z; //playerRotation
+            stateRep[27] = player.GetComponent<PlayerHealth>().health; //playerHealth
+            stateRep[28] = player.GetComponent<PlayerController>().dashing ? 1 : 0; //playerIsDashing
+            stateRep[29] = player.GetComponent<PlayerController>().dashOnCD ? 1 : 0; //playerTriesDashOnCD
+
+            float cursorDistanceFromPlayer = (new Vector2(cursor.transform.position.x, cursor.transform.position.y) -
+               new Vector2(player.transform.position.x, player.transform.position.y)).magnitude;
+            stateRep[21] = cursorDistanceFromPlayer; //cursorDistanceFromPlayer
+
+            stateRep[40] = player.GetComponent<PlayerHealth>().isBurning ? 1 : 0; //playerBurning
+            stateRep[41] = player.GetComponent<PlayerHealth>().isHealing ? 1 : 0; //playerHealing
+            stateRep[42] = player.GetComponent<PlayerHealth>().deltaHealth; //playerDeltaHealth
+            stateRep[43] = player.GetComponent<PlayerHealth>().playerDied ? 1 : 0; //playerDied
+        }
+
+        if (!player.GetComponent<PlayerController>().isAI)
+        {
+            //dashPressed
+            stateRep[30] = ((Input.GetKeyDown(KeyCode.LeftShift) ||
+                Input.GetKeyDown(KeyCode.RightShift) ||
+                Input.GetKeyDown(KeyCode.Space)) ? 1 : 0);
+
+            //playerTriesToFireOnCD
+            stateRep[34] =(Input.GetMouseButton(0) &&
+                GameObject.Find("GunControls").GetComponent<GunControls>().projectileCount == 0 ? 1 : 0); 
+            
+            //playerTriesToBombOnCD
+            stateRep[35] = ((Input.GetMouseButtonUp(1) &&
+            cursor.GetComponent<GunControls>().bombCount == 0 ? 1 : 0));
+        }
+        else
+        {
+            stateRep[30] = 0.0f;
+            stateRep[34] = 0.0f; //playerTriesToFireOnCD
+            stateRep[35] = 0.0f; //playerTriesToBombOnCD
+        }
+             
+  
+        
+        stateRep[31] = (cursor.transform.position - previousCursorPosition).magnitude; //cursorDistanceTraveled
+        previousCursorPosition = cursor.transform.position;
+
+
+        stateRep[32] = cursor.transform.position.x; //cursorPositionX
+        stateRep[33] = cursor.transform.position.y; //cursorPositionY
+
+
+        stateRep[36] =
+            !GameObject.Find("GunControls").GetComponent<GunControls>().reloading ? 1 : 0; //shotsFired
+
+        stateRep[37] =
+            !GameObject.Find("GunControls").GetComponent<GunControls>().bombReloading ? 1 : 0; //bombDropped
+
+        stateRep[38] = 
+            GameObject.Find("GunControls").GetComponent<GunControls>().reloading ? 1 : 0; //gunReloading
+
+        stateRep[39] = 
+            GameObject.Find("GunControls").GetComponent<GunControls>().bombReloading ? 1 : 0; //bombReloading
+
+        stateRep[49] = GameObject.FindGameObjectsWithTag("fire").Length; //onScreenFires
+        stateRep[50] = GameObject.FindGameObjectsWithTag("projectile").Length; //onScreenBullets
+
+
+        stateRep[44] = agent.GetComponent<FrustrationComponent>().targetWasVisible &&
+            !agent.GetComponent<Movement>().targetLastSeen ? 1 : 0; //botLostPlayer
+
+
+        stateRep[45] = !agent.GetComponent<FrustrationComponent>().targetWasVisible &&
+            agent.GetComponent<Movement>().targetLastSeen ? 1 : 0; //botSpottedPlayer
+
+
+        stateRep[51] = keyPresses.Count; //keyPressCount
+        stateRep[52] = timeLimit - countdown; //[general]timePassed
+        stateRep[53] = keyPresses.Count; //[general]inputIntensity
+        stateRep[54] = keyPresses.Distinct().Count(); //[general]inputDiversity
+        stateRep[55] = 1 - ((float)idleTime / (float)fullTime); //[general]activity
+        stateRep[56] = score; //[general]score
+
+        return stateRep;
+    }
+
+    //======================TIME MANAGEMENT============================
+
+    private static readonly DateTime Jan1st1970 = new DateTime
+    (1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    public static long CurrentTimeMillis()
+    {
+        return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
+    }
+
+    private long ReturnEpochStamp()
+    {
+        return (long)(DateTime.UtcNow - Jan1st1970).TotalMilliseconds;
     }
 }
